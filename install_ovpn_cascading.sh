9d1693477eefcb289cc2027a6f57586ee65f5139
PrivateMemberPP <59559223+PrivateMemberPP@users.noreply.github.com>
2020-01-09 12:55:20 +0100

    Update install_ovpn_cascading.sh

--- install_ovpn_cascading.sh
+++ install_ovpn_cascading.sh
@@ -99,8 +99,17 @@ wget -q -P $curdir'/OVPN_SWITCH/' $DL_CASC_SCR > /dev/null
 FILE_DL_CASC_SCR=($(echo $DL_CASC_SCR | rev | cut -d '/' -f 1 | rev))
 
 # falls ein Update durchgefuehrt wird, erstmal die Dienste beenden
-systemctl stop openvpn-restart-cascading.service > /dev/null
-systemctl stop openvpn-restart-cascading-watchdog.service > /dev/null
+systemctl --full --type service --all | grep openvpn-restart-cascading.service
+if [ $? -eq "0" ];
+then
+	systemctl stop openvpn-restart-cascading.service > /dev/null
+fi
+
+systemctl --full --type service --all | grep openvpn-restart-cascading-watchdog.service
+if [ $? -eq "0" ];
+then
+	systemctl stop openvpn-restart-cascading-watchdog.service > /dev/null
+fi
 
 sleep 2
 
