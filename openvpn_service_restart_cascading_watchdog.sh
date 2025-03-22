0e11256ca4d822883176e4a0649119a1e8769657
PrivateMemberPP <59559223+PrivateMemberPP@users.noreply.github.com>
2025-03-22 15:04:57 +0100

    Update openvpn_service_restart_cascading_watchdog.sh

--- openvpn_service_restart_cascading_watchdog.sh
+++ openvpn_service_restart_cascading_watchdog.sh
@@ -6,96 +6,94 @@
 folder_logpath=/var/log/ovpn_reconnect/
 #
 # Logfile fuer dieses Watchdog-Script
-logfile_watchdog="$folder_logpath"watchdog_openvpn_reconnect.log
+logfile_watchdog=/var/log/ovpn_reconnect/watchdog_openvpn_reconnect.log
 #
 # Checkfile fuer den Watchdog-Service
-checkfile_watchdog="$folder_logpath"exitnode.log
+checkfile_watchdog=/var/log/ovpn_reconnect/exitnode.log
 #
 ### ENDE Variablen deklarieren ###
 #
 ### Definition von Funktionen ###
 #
 function checkfile {
-	if [ -f "$checkfile_watchdog" ];
-	then
-		chkfl="1"
-	else
-		chkfl="0"
-	fi
+        if [ -f "$checkfile_watchdog" ];
+        then
+                chkfl="1"
+        else
+                chkfl="0"
+        fi
 }
 function get_state {
-	current_state=$(cat $checkfile_watchdog)
-	sleep 1
+        sleep 4
+        current_state=$(cat $checkfile_watchdog)
+        sleep 0.1
 }
 function check_inactivity {
-	if grep "Inactivity timeout (--ping-restart), restarting" "$folder_logpath"log.vpnhop*
-	then
-		{
-			echo -e "\n----------ACHTUNG----------"
-			echo -e "Es ist jetzt $(date)"
-			echo -e "Mindestens ein Server der Kaskade ist nicht mehr erreichbar!"
-			echo -e "Dienste nun neustarten, damit ein sicherer Zustand wiederhergestellt werden kann!"
-		} >> $logfile_watchdog
-		kill_primary_process
-	fi
+        if grep "Inactivity timeout (--ping-restart), restarting" "$folder_logpath"log.vpnhop*
+        then
+                {
+                        echo -e "\n----------ACHTUNG----------"
+                        echo -e "Es ist jetzt $(date)"
+                        echo -e "Mindestens ein Server der Kaskade ist nicht mehr erreichbar!"
+                        echo -e "Dienste nun neustarten, damit ein sicherer Zustand wiederhergestellt werden kann!"
+                } >> $logfile_watchdog
+                kill_primary_process
+        fi
 }
 function check_state {
-	wget -O - -q --tries=3 --timeout=20 https://checkip.perfect-privacy.com/json | cut -d '"' -f 8 | grep "$current_state" >> /dev/null
-	RET=$?
-	pub_ip=$(wget -O - -q --tries=3 --timeout=20 https://checkip.perfect-privacy.com/json | cut -d '"' -f 8)
-	sleep 4
+        wget -O - -q --tries=3 --timeout=20 https://checkip.perfect-privacy.com/json | cut -d '"' -f 8 | grep "$current_state" >> /dev/null
+        RET=$?
+        pub_ip=$(wget -O - -q --tries=3 --timeout=20 https://checkip.perfect-privacy.com/json | cut -d '"' -f 8)
+        sleep 1
 }
 function cleanup {
-	sudo killall openvpn
-	sleep 2
-	sudo tmux kill-server
-	sleep 0.5
+        sudo killall openvpn
+        sudo tmux kill-server
 }
 function kill_primary_process {
-	cleanup
-	PID=$(sudo systemctl --property="MainPID" show openvpn-restart-cascading.service | cut -d '=' -f 2)
-	sleep 0.2
-	sudo kill -9 "$(ps -o pgid= "$PID" | grep -o '[0-9]*')" > /dev/null
+        cleanup
+        PID=$(sudo systemctl --property="MainPID" show openvpn-restart-cascading.service | cut -d '=' -f 2)
+        sudo kill -9 "$(ps -o pgid= "$PID" | grep -o '[0-9]*')" > /dev/null
 }
 function log_delete {
-	if [[ "$(wc -c $logfile_watchdog | cut -d ' ' -f 1)" -gt "20480" ]];
-	then
-		echo "" > $logfile_watchdog
-	fi
+        if [[ "$(wc -c $logfile_watchdog | cut -d ' ' -f 1)" -gt "20480" ]];
+        then
+                echo "" > $logfile_watchdog
+        fi
 }
 function continuously_check {
-	while [ -f "$checkfile_watchdog" ]
-	do
-		# aktuellen Dateiinhalt in eine Variable speichern
-		get_state
+        while [ -f "$checkfile_watchdog" ]
+        do
+                # aktuellen Dateiinhalt in eine Variable speichern
+                get_state
 
-		echo -e "\n\nVerbindung besteht seit:\t\t$(date)" >> $logfile_watchdog
-		echo -e "mit oeffentlicher IP:\t\t\t$current_state" >> $logfile_watchdog
+                echo -e "\n\nVerbindung besteht seit:\t\t$(date)" >> $logfile_watchdog
+                echo -e "mit oeffentlicher IP:\t\t\t$current_state" >> $logfile_watchdog
 
-		check_inactivity
-		check_state
+                check_inactivity
+                check_state
 
-		while [ $RET -eq "0" ]
-		do
-			check_inactivity
-			get_state
-			check_state
-		done
+                while [ $RET -eq "0" ]
+                do
+                        check_inactivity
+                        get_state
+                        check_state
+                done
 
-		get_state
-		if [ ! "$current_state" == "Warten" ];
-		then
-			{
-				echo -e "\n----------ACHTUNG----------"
-				echo -e "Es ist jetzt $(date)"
-				echo -e "Oeffentliche IP hat sich geaendert!"
-				echo -e "Dienste nun neustarten, damit ein sicherer Zustand wiederhergestellt werden kann!"
-			} >> $logfile_watchdog
-			kill_primary_process
-			sudo rm $checkfile_watchdog
-		fi
-		return
-	done
+                get_state
+                if [ ! "$current_state" == "Warten" ];
+                then
+                        {
+                                echo -e "\n----------ACHTUNG----------"
+                                echo -e "Es ist jetzt $(date)"
+                                echo -e "Oeffentliche IP hat sich geaendert!"
+                                echo -e "Dienste nun neustarten, damit ein sicherer Zustand wiederhergestellt werden kann!"
+                        } >> $logfile_watchdog
+                        kill_primary_process
+                        sudo rm $checkfile_watchdog
+                fi
+                return
+        done
 }
 #
 ### ENDE Definition von Funktionen ###
@@ -105,39 +103,39 @@ timeout=0
 
 while true
 do
-	# Wenn das LOG groesser als 20MB ist, dieses leeren
-	log_delete
+        # Wenn das LOG groesser als 20MB ist, dieses leeren
+        log_delete
 
-	checkfile
+        checkfile
 
-	case "$chkfl" in
-		# Datei existiert und kann kontinuierlich ausgewertet werden
-		1)
-			get_state
-			case "$current_state" in
-				Warten)
-					sleep 5
-					timeout=0
-					;;
-				*)
-					continuously_check
-					timeout=0
-					;;
-			esac
-			;;
+        case "$chkfl" in
+                # Datei existiert und kann kontinuierlich ausgewertet werden
+                1)
+                        get_state
+                        case "$current_state" in
+                                Warten)
+                                        sleep 0.1
+                                        timeout=0
+                                        ;;
+                                *)
+                                        continuously_check
+                                        timeout=0
+                                        ;;
+                        esac
+                        ;;
 
-		# Datei existiert noch nicht, erneut pruefen
-		0)
+                # Datei existiert noch nicht, erneut pruefen
+                0)
 
-			sleep 2
-			timeout=$(("timeout" + "2"))
+                        sleep 0.1
+                        timeout=$(("timeout" + "0.1"))
 
-			# falls nach einem gewissen Counter die Datei noch nicht existiert, stimmt irgendwas nicht
-			# fuer diesen Fall den primaeren Prozess neustarten
-			if [ "$timeout" -eq "10" ]
-			then
-				kill_primary_process
-			fi
-			;;
-	esac
+                        # falls nach einem gewissen Counter die Datei noch nicht existiert, stimmt irgendwas nicht
+                        # fuer diesen Fall den primaeren Prozess neustarten
+                        if [ "$timeout" -eq "10" ]
+                        then
+                                kill_primary_process
+                        fi
+                        ;;
+        esac
 done
